e4a9dc947d04e873a60ed92c3d0f19b2765b2695
Julie Rodriguez <jurodriguez@Julies-MacBook-Pro.local>
2018-10-10 10:48:42 -0400

    Finished To-Do List react tutorial

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,4 +1,6 @@
 import React, {Component} from "react";
+import TodoItems from "./TodoItems";
+import "./TodoList.css";
 
 class TodoList extends Component {
     constructor(props) {
@@ -9,6 +11,7 @@ class TodoList extends Component {
         };
 
         this.addItem = this.addItem.bind(this);
+        this.deleteItem = this.deleteItem.bind(this);
     }
 
     addItem(e) {
@@ -32,6 +35,15 @@ class TodoList extends Component {
         e.preventDefault();
     }
 
+    deleteItem(key) {
+        var filteredItems = this.state.items.filter(function (item) {
+            return (item.key !== key);
+        });
+        this.setState ({
+            items: filteredItems
+        });
+    }
+
     render() {
         return (
             <div className="todoListMain">
@@ -43,6 +55,8 @@ class TodoList extends Component {
                         <button type="submit">add</button>
                     </form>
                 </div>
+                <TodoItems entries={this.state.items}
+                            delete={this.deleteItem}/>
             </div>
         );
     }
